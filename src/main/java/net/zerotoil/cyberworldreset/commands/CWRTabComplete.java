c480d883023e1716b8084d93d52124dfcdd3bf24
Kihsomray <zerotoil.network@gmail.com>
2022-01-12 19:30:36 -0800

    Fixed Issue With Tab-complete (0.5.6.1)

--- src/main/java/net/zerotoil/cyberworldreset/commands/CWRTabComplete.java
+++ src/main/java/net/zerotoil/cyberworldreset/commands/CWRTabComplete.java
@@ -23,6 +23,7 @@ public class CWRTabComplete implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!(sender instanceof Player)) return null;
         Player player = (Player) sender;
         //String uuid = player.getUniqueId().toString();
 
